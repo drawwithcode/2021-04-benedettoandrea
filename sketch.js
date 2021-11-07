60597c2a3bedceb23283bc45711ff833c9e6e599
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2021-11-07 22:18:36 +0100

    further development

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
-let wrapper;
+let agreeAndSave;
+let learnMore;
 let privacyStatement;
-let button;
+let agreeAndSaveEmoji;
+let learnMoreEmoji;
 
 function setup() {
   createCanvas(windowWidth, windowHeight).parent("container");
@@ -13,64 +15,68 @@ function draw() {
   clear();
   push();
   noStroke();
-  fill("white");
+  text("culo", 0, 0);
+  fill("rgb(13, 71, 161)");
   rect(
     width / 2,
     height / 2,
     windowWidth * sin(frameCount / 500),
-    windowHeight * sin(frameCount / 500)
+    windowHeight * sin(frameCount / 500),
+    5
   );
   pop();
-  line(0, 0, width, height);
-  line(width, 0, 0, height);
-  wrapper = select(".wrapper");
+  // line(0, 0, width, height);
+  // line(width, 0, 0, height);
+  agreeAndSave = select("#agreeAndSave");
+  agreeAndSaveEmoji = select("#agreeAndSaveEmoji");
+  learnMore = select("#learnMore");
+  learnMoreEmoji = select("#learnMoreEmoji");
   privacyStatement = select(".privacyStatement");
-  button = select("#learnMore");
-  button.mouseOver(learnMoreToggleOn);
-  button.mouseOut(learnMoreToggleOff);
+  agreeAndSave.mouseOver(() => {
+    agreeAndSaveEmoji.class("emoji show");
+  });
+  agreeAndSave.mouseOut(() => {
+    agreeAndSaveEmoji.class("emoji hide");
+  });
+  learnMore.mouseOver(() => {
+    learnMoreEmoji.class("emoji show");
+  });
+  learnMore.mouseOut(() => {
+    learnMoreEmoji.class("emoji hide");
+  });
+
+  agreeAndSave.touchStarted(() => {
+    console.log("start");
+  });
+  agreeAndSave.touchMoved(() => {
+    console.log("move");
+  });
+  agreeAndSave.touchEnded(() => {
+    console.log("end");
+  });
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function hideWrapper() {
-  wrapper.addClass("hide");
-}
-
-let learnMoreSelector = false;
-function learnMore() {
-  if (
-    privacyStatement.class() == "privacyStatement hide" &&
-    learnMoreSelector == false
-  ) {
-    learnMoreSelector = true;
-    console.log(learnMoreSelector);
-    privacyStatement.removeClass("hide");
-    privacyStatement.addClass("show");
-  } else if (
-    privacyStatement.class() == "privacyStatement show" &&
-    learnMoreSelector == true
-  ) {
-    learnMoreSelector = false;
-    console.log(learnMoreSelector);
-    privacyStatement.removeClass("show");
-    privacyStatement.addClass("hide");
+function learnMoreToggle() {
+  if (privacyStatement.class() == "privacyStatement hide") {
+    privacyStatement.class("privacyStatement show");
+  } else if (privacyStatement.class() == "privacyStatement show") {
+    privacyStatement.class("privacyStatement hide");
   }
 }
 
-function learnMoreToggleOn() {
-  if (learnMoreSelector === false) {
-    //console.log(learnMoreSelector);
-    privacyStatement.removeClass("hide");
-    privacyStatement.addClass("show");
-  }
+function agreeAndSaveEmojiShow() {
+  agreeAndSaveEmoji.class("emoji show");
 }
-
-function learnMoreToggleOff() {
-  if (learnMoreSelector === false) {
-    // console.log(learnMoreSelector);
-    privacyStatement.removeClass("show");
-    privacyStatement.addClass("hide");
-  }
+function agreeAndSaveEmojiHide() {
+  agreeAndSaveEmoji.class("emoji hide");
+}
+function learnMoreEmojiShow() {
+  learnMoreEmoji.class("emoji show");
+}
+function learnMoreEmojiHide() {
+  learnMoreEmoji.class("emoji hide");
 }
